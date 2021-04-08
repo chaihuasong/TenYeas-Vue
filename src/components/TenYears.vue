e2efa27bc4f06fdee4a0f96119454bc2c38c93e1
chaihuasong <chaihs@mob.com>
2021-04-08 16:05:45 +0800

    modify

--- src/components/TenYears.vue
+++ src/components/TenYears.vue
@@ -6,40 +6,29 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h5>请选择您的性别</h5>
-    <ul>
-      <li>
-        <el-radio-group v-model="radio">
-          <el-radio label="1">男</el-radio>
-          <el-radio label="0">女</el-radio>
-        </el-radio-group>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <h5>请输入您的手机号</h5>
-      </li>
-      <el-input placeholder="Please input" prefix-icon="el-icon-mobile-phone" v-model="input" style="width:30%;"></el-input>
-    </ul>
-    <ul>
-      <li>
-        <h5>请输入您的身份证号</h5>
-      </li>
-      <el-input placeholder="Please input" prefix-icon="el-icon-s-custom" v-model="input" style="width:30%;"></el-input>
-    </ul>
-    <h5 style="margin-right: 30%">请输入立志信息</h5>
-    <ul>
-      <el-input
-          type="textarea"
-          :rows="5"
-          style="width:40%;"
-          placeholder="Please input"
-          maxlength="999"
-          show-word-limit
-          v-model="textarea">
-      </el-input>
-    </ul>
+    <div>请选择您的性别</div>
+    <el-radio-group v-model="radio">
+      <el-radio label="1">男</el-radio>
+      <el-radio label="0">女</el-radio>
+    </el-radio-group>
+    <div>请输入您的手机号</div>
+    <el-input placeholder="Please input" prefix-icon="el-icon-mobile-phone" v-model="input" style="width:50%;"></el-input>
+    <div>请输入您的身份证号</div>
+    <el-input placeholder="Please input" prefix-icon="el-icon-s-custom" v-model="input"></el-input>
+    <div style="margin-left: 10px">请输入立志信息</div>
+    <el-input
+        type="textarea"
+        :rows="5"
+        placeholder="Please input"
+        maxlength="999"
+        show-word-limit
+        v-model="textarea">
+    </el-input>
+    <br/>
     <el-button type="primary">提交</el-button>
+    <br/>
+    <br/>
+    <br/>
     <ul>
       <li><a href="http://htz.org.cn" target="_blank" rel="noopener">黄庭禅台湾</a></li>
       <li><a href="https://neixinchan.com" target="_blank" rel="noopener">黄庭禅官网</a></li>
@@ -64,9 +53,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
+
 ul {
   list-style-type: none;
   padding: 0;
