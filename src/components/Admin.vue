74e89b082988f2e2054dc3f1d0d7647b7c2ecb24
chaihuasong <chaihuasong@gmail.com>
2021-04-26 20:24:52 +0800

    侧边栏折叠功能

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <el-container style="height: 100%; border: 1px solid #eee">
-      <el-aside width="10%" style="background-color: rgb(238, 241, 246)">
-        <el-menu :default-openeds="['1', '2']" :default-active="$route.path" router>
+      <el-aside width="auto"
+                @mouseenter.native="collapseOpen"
+                @mouseleave.native="collapseClose"
+                style="background-color: rgb(238, 241, 246);">
+        <el-menu :collapse="isCollapse" :default-active="$route.path" router>
           <el-submenu index="1">
             <template slot="title"><i class="el-icon-menu"></i>导航</template>
             <el-menu-item-group>
@@ -22,15 +25,18 @@
       </el-aside>
 
       <el-container>
-        <el-header style="text-align: right; font-size: 12px">
-          <span style="text-align: center; font-size: 30px; margin-right: 45%">管理后台</span>
-          <el-dropdown>
-            <i class="el-icon-setting" style="margin-right: 15px"></i>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item @click.native="gotoLogin">退出</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-          <span>Admin</span>
+        <el-header style="text-align: left; font-size: 12px">
+          <i @click="collapseStatus" class="el-icon-menu"></i>
+          <span style="text-align: center; font-size: 30px; margin-left: 45%">管理后台</span>
+          <div style="float: right">
+            <el-dropdown>
+              <i class="el-icon-setting" style="margin-right: 15px; font-size: 20px"></i>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item @click.native="gotoLogin">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+            <span style="font-size: 16px; color: #3535c6">Admin</span>
+          </div>
         </el-header>
 
         <el-main>
@@ -44,7 +50,25 @@
 
 <script>
 export default {
+  data() {
+    return {
+      collapseBtnClick: false,
+      isCollapse: true,
+    }
+  },
   methods: {
+    collapseStatus() {
+      this.collapseBtnClick = this.isCollapse;
+      this.isCollapse = !this.isCollapse;
+    },
+    collapseOpen() {
+      if (this.collapseBtnClick) return;
+      this.isCollapse = false;
+    },
+    collapseClose() {
+      if (this.collapseBtnClick) return;
+      this.isCollapse = true;
+    },
     gotoLogin() {
       this.$store.commit('$_setStorage', "")
       this.$router.push("/admin")
