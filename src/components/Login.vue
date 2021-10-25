6bf3fd1d828be5a7125a60dcd00c51fa5b45439d
chaihuasong <chaihuasong@gmail.com>
2021-10-25 21:26:01 +0800

    Revert "修正"
    This reverts commit 10dfee320645abf9efcdeb946ddf1b521596bfd0.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -22,7 +22,7 @@ export default {
     getData() {
       //window.location.href = "https://htzchina.org:8081/#/redirect?code=1234"
       //encodeURI() // TODO
-      window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx83aec75c3ca58f0e&redirect_uri=http://htzchina.org/wc_redirect&response_type=code&scope=snsapi_base&state=123#wechat_redirect"
+      window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx83aec75c3ca58f0e&redirect_uri=https://htzchina.org/wc_redirect&response_type=code&scope=snsapi_base&state=123#wechat_redirect"
     }
   }
 };
